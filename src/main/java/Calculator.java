3cec648a0a5287f8d4daa9d224fdbaa02279d3a0
niraj-gupta <niraj.gupta@metacube.com>
2021-01-03 15:52:55 +0530

    [add] initial commits

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,54 @@
+/**
+ * Class to perform basic calculator operations.
+ *
+ * @author niraj
+ */
+public class Calculator {
+  /**
+   * to add two integers
+   *
+   * @param value1
+   * @param value2
+   * @return integer
+   * @author niraj
+   */
+  public int add(int value1, int value2) {
+    return value1 + value2;
+  }
+
+  /**
+   * to subtract two integers
+   *
+   * @param value1
+   * @param value2
+   * @return integer
+   * @author niraj
+   */
+  public int subtract(int value1, int value2) {
+    return value1 - value2;
+  }
+
+  /**
+   * to multiply two integers
+   *
+   * @param value1
+   * @param value2
+   * @return integer
+   * @author niraj
+   */
+  public int multiply(int value1, int value2) {
+    return value1 * value2;
+  }
+
+  /**
+   * to divide two integers
+   *
+   * @param value1
+   * @param value2
+   * @return integer
+   * @author niraj
+   */
+  public int divide(int value1, int value2) {
+    return value1 / value2;
+  }
+}
